9fed7e33c343ba6c4e8027a808e631126606f261
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-08-29 12:55:34 +0200

    First Hooks. Guitar component is dynamic. Add guitars to Carrito

--- 02-guitarla/src/App.jsx
+++ 02-guitarla/src/App.jsx
@@ -1,14 +1,58 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 function App() {
+   const [data, setData] = useState(db);
+   const [cart, setCart] = useState([]);
+
+   // useEffect(() => {
+   //    setData(db);
+   // }, []);
+
+   // * Versión función clásica
+   // function addToCart(item) {
+   //    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+   //    if (itemExists >= 0) {
+   //       const updatedCart = [...cart];
+   //       updatedCart[itemExists].quantity++;
+   //       setCart(updatedCart);
+   //    } else {
+   //       item.quantity = 1;
+   //       setCart([...cart, item]);
+   //    }
+   // }
+
+   // * Versión Arrow Function
+   const addToCart = (item) => {
+      const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+      if (itemExists >= 0) {
+         const updatedCart = [...cart];
+         updatedCart[itemExists].quantity++;
+         setCart(updatedCart);
+      } else {
+         item.quantity = 1;
+         setCart([...cart, item]);
+      }
+   };
+
    return (
       <>
-         <Header />
+         <Header cart={cart} />
          <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
             <div className="row mt-5">
-               <Guitar />
+               {data.map((guitar) => (
+                  <Guitar
+                     key={guitar.id}
+                     guitar={guitar}
+                     setCart={setCart}
+                     addToCart={addToCart}
+                  />
+               ))}
             </div>
          </main>
          <footer className="bg-dark mt-5 py-5">
--- 02-guitarla/src/components/Guitar.jsx
+++ 02-guitarla/src/components/Guitar.jsx
@@ -1,25 +1,29 @@
-function Guitar() {
+import Proptypes from "prop-types";
+
+function Guitar({ guitar, addToCart }) {
+   const { id, name, image, description, price } = guitar;
+
    return (
       <>
          <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
                <img
                   className="img-fluid"
-                  src="./public/img/guitarra_01.jpg"
+                  src={`/img/${image}.jpg`}
                   alt="imagen guitarra"
                />
             </div>
             <div className="col-8">
                <h3 className="text-black fs-4 fw-bold text-uppercase">
-                  Lukather
+                  {name}
                </h3>
-               <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Sit quae labore odit magnam in autem nesciunt, amet
-                  deserunt
-               </p>
-               <p className="fw-black text-primary fs-3">$299</p>
-               <button type="button" className="btn btn-dark w-100">
+               <p>{description}</p>
+               <p className="fw-black text-primary fs-3">{`$ ${price}`}</p>
+               <button
+                  type="button"
+                  className="btn btn-dark w-100"
+                  onClick={() => addToCart(guitar)}
+               >
                   Agregar al Carrito
                </button>
             </div>
@@ -27,5 +31,16 @@ function Guitar() {
       </>
    );
 }
+
+Guitar.propTypes = {
+   addToCart: Proptypes.any,
+   guitar: Proptypes.shape({
+      id: Proptypes.number,
+      name: Proptypes.string,
+      image: Proptypes.string,
+      description: Proptypes.string,
+      price: Proptypes.number,
+   }).isRequired,
+};
 
 export default Guitar;
