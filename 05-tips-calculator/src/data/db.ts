9c9747c1264733020266da0ba1f2aa61183355b3
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-09-02 17:03:02 +0200

    Project Tips begins

---
+++ 05-tips-calculator/src/data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from "../types";
+
+export const menuItems: MenuItem[] = [
+   {
+      id: 1,
+      name: "Pizza a la Leña pequeña",
+      price: 15,
+   },
+   {
+      id: 2,
+      name: "Pizza a la Leña mediana",
+      price: 20,
+   },
+   {
+      id: 6,
+      name: "Pizza a la Leña grande",
+      price: 25,
+   },
+   {
+      id: 7,
+      name: "Rib Eye 800g",
+      price: 35,
+   },
+   {
+      id: 3,
+      name: "Porción de Tarta de Limón",
+      price: 10,
+   },
+   {
+      id: 4,
+      name: "Porción de Pastel de Chocolate",
+      price: 12,
+   },
+   {
+      id: 10,
+      name: "Porción de Tarta de Queso",
+      price: 14,
+   },
+   {
+      id: 5,
+      name: "Zumo de Naranja",
+      price: 8,
+   },
+   {
+      id: 8,
+      name: "Agua con gas",
+      price: 6,
+   },
+   {
+      id: 9,
+      name: "Cerveza",
+      price: 10,
+   },
+   {
+      id: 11,
+      name: "Café Americano",
+      price: 2,
+   },
+   {
+      id: 12,
+      name: "Café Capuchino",
+      price: 4,
+   },
+];
