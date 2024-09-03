cd6bc4fe05df6fa46e2acaef490c71dba5bc67e2
Sergio Díez de Pedro <sergio.diez@softtek.com>
2024-09-03 17:08:04 +0200

    wip: Calories Tracker Project

---
+++ 06-calorie-tracker/src/reducers/activity-reducer.ts
@@ -0,0 +1,37 @@
+import { Activity } from "../types";
+
+export type ActivityActions =
+   | { type: "save-activity"; payload: { newActivity: Activity } }
+   | { type: "set-activeId"; payload: { id: Activity["id"] } };
+
+type ActivityState = {
+   activities: Activity[];
+   activeId: Activity["id"];
+};
+
+export const initialState: ActivityState = {
+   activities: [],
+   activeId: "",
+};
+
+export const activityReducer = (
+   state: ActivityState = initialState,
+   action: ActivityActions
+) => {
+   if (action.type === "save-activity") {
+      // * El código siguiente maneja la lógica para actualizar el 'state'
+      return {
+         ...state,
+         activities: [...state.activities, action.payload.newActivity],
+      };
+   }
+
+   if (action.type === "set-activeId") {
+      return {
+         ...state,
+         activeId: action.payload.id,
+      };
+   }
+
+   return state;
+};
