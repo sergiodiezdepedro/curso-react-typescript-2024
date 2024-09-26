62b597f19c0d5c384d21bfa90e82f0b496a31680
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-09-26 17:07:47 +0200

    wip Clima App

--- 12-clima-react/src/hooks/useWeather.ts
+++ 12-clima-react/src/hooks/useWeather.ts
@@ -9,8 +9,10 @@ export default function useWeather() {
          const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
          const { data } = await axios(geoUrl);
+
          const lat = data[0].lat;
          const lon = data[0].lon;
+
          const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
 
          const { data: weatherResult } = await axios(weatherUrl);
