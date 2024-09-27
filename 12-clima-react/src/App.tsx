e3965577b21ce91bebb549c888985c27e2f61058
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-09-27 12:22:00 +0200

    Clima App finished

--- 12-clima-react/src/App.tsx
+++ 12-clima-react/src/App.tsx
@@ -1,18 +1,22 @@
 import styles from "./App.module.css";
+import Alert from "./components/Alert/Alert";
 import Form from "./components/Form/Form";
+import Spinner from "./components/Spinner/Spinner";
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
 import useWeather from "./hooks/useWeather";
 
 function App() {
-   const { fetchWeather } = useWeather();
+   const { weather, loading, notFound, fetchWeather, hasWeatherData } =
+      useWeather();
 
    return (
       <>
          <h1 className={styles.title}>Buscador de Clima</h1>
          <div className={styles.container}>
-            <p>
-               <Form fetchWeather={fetchWeather} />
-            </p>
-            <p>2</p>
+            <Form fetchWeather={fetchWeather} />
+            {loading && <Spinner />}
+            {hasWeatherData && <WeatherDetail weather={weather} />}
+            {notFound && <Alert>Ciudad no encontrada</Alert>}
          </div>
       </>
    );
--- 12-clima-react/src/hooks/useWeather.ts
+++ 12-clima-react/src/hooks/useWeather.ts
@@ -1,28 +1,99 @@
+import { useMemo, useState } from "react";
 import axios from "axios";
+import { z } from "zod";
 import { SearchType } from "../types";
 
+// * Type guards o Assertions
+// function isWeatherResponse(weather: unknown): weather is Weather {
+//    return (
+//       Boolean(weather) &&
+//       typeof weather === "object" &&
+//       typeof (weather as Weather).name === "string" &&
+//       typeof (weather as Weather).main.temp === "number" &&
+//       typeof (weather as Weather).main.temp_max === "number" &&
+//       typeof (weather as Weather).main.temp_min === "number"
+//    );
+// }
+
+// * Zod
+const Weather = z.object({
+   name: z.string(),
+   main: z.object({
+      temp: z.number(),
+      temp_max: z.number(),
+      temp_min: z.number(),
+   }),
+});
+
+export type Weather = z.infer<typeof Weather>;
+
+const initialState = {
+   name: "",
+   main: {
+      temp: 0,
+      temp_max: 0,
+      temp_min: 0,
+   },
+};
+
 export default function useWeather() {
+   const [weather, setWeather] = useState<Weather>(initialState);
+   const [loading, setLoading] = useState(false);
+   const [notFound, setNotFound] = useState(false);
+
    const fetchWeather = async (search: SearchType) => {
       const appId = import.meta.env.VITE_APY_KEY;
 
+      setLoading(true);
+      setWeather(initialState);
+      setNotFound(false);
+
       try {
-         const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+         const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
          const { data } = await axios(geoUrl);
+
+         // * Comprobar si existe la ciudad
+         if (!data[0]) {
+            setNotFound(true);
+            return;
+         }
 
          const lat = data[0].lat;
          const lon = data[0].lon;
 
          const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
 
-         const { data: weatherResult } = await axios(weatherUrl);
+         // * El código que se implementaría si se utilizan los Type guards de arriba
+         // const result = isWeatherResponse(weatherResult);
+         // if (result) {
+         //    console.log(weatherResult.name);
+         // } else {
+         //    console.log("Respuesta mal formada");
+         // }
 
-         console.log(weatherResult);
+         // * Zod
+         const { data: weatherResult } = await axios(weatherUrl);
+         const result = Weather.safeParse(weatherResult);
+         if (result.success) {
+            setWeather(result.data);
+         } else {
+            console.log("Respuesta mal formada");
+         }
       } catch (error) {
          console.log(error);
+      } finally {
+         setLoading(false);
       }
    };
+
+   const hasWeatherData = useMemo(() => weather.name, [weather]);
+
    return {
       fetchWeather,
+      hasWeatherData,
+      loading,
+      notFound,
+      weather,
    };
 }
