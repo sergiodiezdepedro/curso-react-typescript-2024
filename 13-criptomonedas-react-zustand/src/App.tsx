7a710d413aad9bfbf1f607077857d49daa450bc1
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-09-27 13:28:24 +0200

    Cripto App begins

---
+++ 13-criptomonedas-react-zustand/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+   return (
+      <>
+         <div className="container">
+            <h1 className="app-title">
+               Cotizador de <span>Criptomonedas</span>
+            </h1>
+            <section className="content"></section>
+         </div>
+      </>
+   );
+}
+
+export default App;
