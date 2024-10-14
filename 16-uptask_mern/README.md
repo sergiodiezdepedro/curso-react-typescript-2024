21bc0b293c451d31419bcdb5e386654188c583e8
Sergio Díez de Pedro <sergio.diez@softtek.com>
2024-10-14 17:00:43 +0200

    Some minor cosmetic changes

--- 16-uptask_mern/README.md
+++ 16-uptask_mern/README.md
@@ -1,11 +1,31 @@
 # Stack
 
+## Front
+
 -  react-router-dom
 -  react-hook-form
 -  zod
 -  axios
--  React Query o también TanStack Query es una librería para obtener datos del servidor. npm i @tanstack/react-query
+-  React Query, o también conocida TanStack Query, es una librería para obtener datos del servidor. npm i @tanstack/react-query
 -  tailwindcss
 -  @headlessui/react
 -  @heroicons/react
 -  react-toastify
+-  @chakra-ui/pin-input
+
+## Back
+
+-  bcrypt: ofuscar las contraseñas de los usuarios. https://www.npmjs.com/package/bcrypt
+-  colors
+-  cors
+-  dotenv
+-  express
+-  express-validator
+-  mongoose
+-  morgan
+-  nodemailer: Send emails from Node.js. https://www.npmjs.com/package/nodemailer
+-  JSON Web Token. El paquete es jsonwebtoken
+
+## mailtrap
+
+[Email Delivery Platform](https://mailtrap.io/) that delivers just in time. Great for dev, and marketing teams. Sólo para el entorno de desarrollo.
--- 16-uptask_mern/backend/src/emails/AuthEmail.ts
+++ 16-uptask_mern/backend/src/emails/AuthEmail.ts
@@ -1,43 +1,43 @@
-import { transporter } from "../config/nodemailer"
+import { transporter } from "../config/nodemailer";
 
 interface IEmail {
-    email: string
-    name: string
-    token: string
+   email: string;
+   name: string;
+   token: string;
 }
 
 export class AuthEmail {
-    static sendConfirmationEmail = async ( user : IEmail ) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <admin@uptask.com>',
-            to: user.email,
-            subject: 'UpTask - Confirma tu cuenta',
-            text: 'UpTask - Confirma tu cuenta',
-            html: `<p>Hola: ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
+   static sendConfirmationEmail = async (user: IEmail) => {
+      const info = await transporter.sendMail({
+         from: "UpTask <admin@uptask.com>",
+         to: user.email,
+         subject: "UpTask - Confirma tu cuenta",
+         text: "UpTask - Confirma tu cuenta",
+         html: `<p>Hola: ${user.name}, has creado tu cuenta en UpTask, ya casi está todo listo, sólo debes confirmar tu cuenta</p>
                 <p>Visita el siguiente enlace:</p>
                 <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
                 <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        })
+                <p>Este token expira en 10 minutos.</p>
+            `,
+      });
 
-        console.log('Mensaje enviado', info.messageId)
-    }
+      console.log("Mensaje enviado", info.messageId);
+   };
 
-    static sendPasswordResetToken = async ( user : IEmail ) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <admin@uptask.com>',
-            to: user.email,
-            subject: 'UpTask - Reestablece tu password',
-            text: 'UpTask - Reestablece tu password',
-            html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
+   static sendPasswordResetToken = async (user: IEmail) => {
+      const info = await transporter.sendMail({
+         from: "UpTask <admin@uptask.com>",
+         to: user.email,
+         subject: "UpTask - Reestablece tu password",
+         text: "UpTask - Reestablece tu password",
+         html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
                 <p>Visita el siguiente enlace:</p>
                 <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
                 <p>E ingresa el código: <b>${user.token}</b></p>
                 <p>Este token expira en 10 minutos</p>
-            `
-        })
+            `,
+      });
 
-        console.log('Mensaje enviado', info.messageId)
-    }
+      console.log("Mensaje enviado", info.messageId);
+   };
 }
